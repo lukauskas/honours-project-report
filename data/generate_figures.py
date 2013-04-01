c15200bbe36492199800c8646f634ab83603948a
Saulius Lukauskas <luksaulius@gmail.com>
2013-04-01 20:00:17 +0100

    wrote about pileup height normalisation

--- data/generate_figures.py
+++ data/generate_figures.py
@@ -5,20 +5,39 @@ import dgw.dtw.visualisation
 import matplotlib.pyplot as plt
 
 
-regions = dgw.read_bed('macs_interesting.bed')
-data = dgw.read_bam(['/Users/saulius/dev/coursework/proj/data/interesting/broad/K562/wgEncodeBroadHistoneK562H3k4me3StdAlnRep1.bam'], regions, resolution=50, extend_to=200)
+regions = dgw.read_bed('interesting_tss_regions.bed')
+data = dgw.read_bam(['/Users/saulius/dev/coursework/proj/data/interesting/broad/K562/wgEncodeBroadHistoneK562H3k4me3StdAlnRep1.bam'], regions, resolution=25, extend_to=200)
 
 def visualise_dtw_mappings(index_a, index_b, *args, **kwargs):
-    a = data.ix[index_a]
-    b = data.ix[index_b]
+    if isinstance(index_a, basestring):
+        a = data.ix[index_a]
+        b = data.ix[index_b]
+        x_label = index_a
+        y_label = index_b
+    else:
+        a = index_a
+        b = index_b
+        del index_a, index_b
+        x_label = ""
+        y_label = ""
 
     dtw_func = dgw.dtw.parametrised_dtw_wrapper(*args, **kwargs)
-    return dgw.dtw.visualisation.visualise_dtw_mappings(a,b, sequence_x_label=index_a, sequence_y_label=index_b, dtw_function=dtw_func)
+    return dgw.dtw.visualisation.visualise_dtw_mappings(a,b, sequence_x_label=x_label, sequence_y_label=y_label, dtw_function=dtw_func)
 
 def visualise_dtw_cost_function(index_a, index_b, *args, **kwargs):
-    a = data.ix[index_a]
-    b = data.ix[index_b]
 
+    if isinstance(index_a, basestring):
+        a = data.ix[index_a]
+        b = data.ix[index_b]
+        x_label = index_a
+        y_label = index_b
+    else:
+        a = index_a
+        b = index_b
+        del index_a, index_b
+        x_label = ""
+        y_label = ""
+    
     dtw_func = dgw.dtw.parametrised_dtw_wrapper(*args, **kwargs)
     return dgw.dtw.visualisation.visualise_dtw(a,b,dtw_function=dtw_func)
 
